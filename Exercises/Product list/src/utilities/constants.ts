3efe7b3427e20ba41f264e5387c749a20e0be1bb
Mario Milosevic <128989997+MarioMilosevic@users.noreply.github.com>
2024-10-12 17:12:27 +0200

    Add constants file

---
+++ Exercises/Product list/src/utilities/constants.ts
@@ -0,0 +1,56 @@
+export const dessertsData = [
+  {
+    name: "Waffle",
+    description: "Waffle with Berries",
+    price: 6.5,
+    image: "../assets/image-waffle-desktop.jpg",
+  },
+  {
+    name: "Creme Brulee",
+    description: "Vanilla Bean Creme Brulee",
+    price: 7,
+    image: "../assets/image-creme-brulee-desktop.jpg",
+  },
+  {
+    name: "Macaron",
+    description: "Macaron Mix of Five",
+    price: 8,
+    image: "../assets/image-macaron-desktop.jpg",
+  },
+  {
+    name: "Tiramisu",
+    description: "Classic Tiramisu",
+    price: 5.5,
+    image: "../assets/image-tiramisu-desktop.jpg",
+  },
+  {
+    name: "Baklava",
+    description: "Pistachio Baklava",
+    price: 4.0,
+    image: "../assets/image-baklava-desktop.jpg",
+  },
+  {
+    name: "Pie",
+    description: "Lemon Meringue Pie",
+    price: 5.0,
+    image: "../assets/image-meringue-desktop.jpg",
+  },
+  {
+    name: "Cake",
+    description: "Red Velvet Cake",
+    price: 4.5,
+    image: "../assets/image-cake-desktop.jpg",
+  },
+  {
+    name: "Brownie",
+    description: "Salted Caramel Brownie",
+    price: 4.5,
+    image: "../assets/image-brownie-desktop.jpg",
+  },
+  {
+    name: "Panna Cotta",
+    description: "Vanilla Panna Cotta",
+    price: 6.5,
+    image: "../assets/image-panna-cotta-desktop.jpg",
+  },
+];
