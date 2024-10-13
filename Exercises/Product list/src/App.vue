66f8d18bb4e8871c46cdc3972e17b9ccf30f0cc1
Mario Milosevic <128989997+MarioMilosevic@users.noreply.github.com>
2024-10-13 14:32:10 +0200

    Render desserts

--- Exercises/Product list/src/App.vue
+++ Exercises/Product list/src/App.vue
@@ -1,17 +1,28 @@
+<template>
+  <ul>
+    <li v-for="dessert in desserts" :key="dessert.name">
+      <img v-bind:src="dessert.image" v-bind:alt="dessert.image" />
+      <div>
+        <h2>{{ dessert.name }}</h2>
+        <h3>{{ dessert.description }}</h3>
+        <h4>{{ dessert.price }}</h4>
+      </div>
+    </li>
+  </ul>
+</template>
+
 <script lang="ts">
-  import { dessertsData } from './utilities/constants';
+import { dessertsData } from "./utilities/constants";
+import SingleDesert from "./components/single-desert.vue";
+console.log(dessertsData);
 export default {
   data() {
     return {
       desserts: dessertsData,
     };
   },
+  components: {
+    SingleDesert,
+  },
 };
 </script>
-
-<template>
-  <div>
-    mario
-    <!-- {{ dessertsData }} -->
-  </div>
-</template>
--- Exercises/Product list/src/utilities/constants.ts
+++ Exercises/Product list/src/utilities/constants.ts
@@ -1,56 +1,66 @@
+import waffleImage from "../assets/image-waffle-desktop.jpg";
+import vanillaBrulle from "../assets/image-creme-brulee-desktop.jpg";
+import macaron from "../assets/image-macaron-desktop.jpg";
+import tiramisu from "../assets/image-tiramisu-desktop.jpg";
+import baklava from "../assets/image-baklava-desktop.jpg";
+import pie from "../assets/image-meringue-desktop.jpg";
+import cake from "../assets/image-cake-desktop.jpg";
+import brownie from "../assets/image-brownie-desktop.jpg";
+import pannaCotta from "../assets/image-panna-cotta-desktop.jpg";
+
 export const dessertsData = [
   {
     name: "Waffle",
     description: "Waffle with Berries",
     price: 6.5,
-    image: "../assets/image-waffle-desktop.jpg",
+    image: waffleImage,
   },
   {
     name: "Creme Brulee",
     description: "Vanilla Bean Creme Brulee",
     price: 7,
-    image: "../assets/image-creme-brulee-desktop.jpg",
+    image: vanillaBrulle,
   },
   {
     name: "Macaron",
     description: "Macaron Mix of Five",
     price: 8,
-    image: "../assets/image-macaron-desktop.jpg",
+    image: macaron,
   },
   {
     name: "Tiramisu",
     description: "Classic Tiramisu",
     price: 5.5,
-    image: "../assets/image-tiramisu-desktop.jpg",
+    image: tiramisu,
   },
   {
     name: "Baklava",
     description: "Pistachio Baklava",
     price: 4.0,
-    image: "../assets/image-baklava-desktop.jpg",
+    image: baklava,
   },
   {
     name: "Pie",
     description: "Lemon Meringue Pie",
     price: 5.0,
-    image: "../assets/image-meringue-desktop.jpg",
+    image: pie,
   },
   {
     name: "Cake",
     description: "Red Velvet Cake",
     price: 4.5,
-    image: "../assets/image-cake-desktop.jpg",
+    image: cake,
   },
   {
     name: "Brownie",
     description: "Salted Caramel Brownie",
     price: 4.5,
-    image: "../assets/image-brownie-desktop.jpg",
+    image: brownie,
   },
   {
     name: "Panna Cotta",
     description: "Vanilla Panna Cotta",
     price: 6.5,
-    image: "../assets/image-panna-cotta-desktop.jpg",
+    image: pannaCotta,
   },
 ];
