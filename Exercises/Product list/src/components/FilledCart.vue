f93a860b99d8d15df4093c05e43ff67628b9d02b
Mario Milosevic <128989997+MarioMilosevic@users.noreply.github.com>
2024-10-15 19:43:53 +0200

    Add confirm butotn

--- Exercises/Product list/src/components/FilledCart.vue
+++ Exercises/Product list/src/components/FilledCart.vue
@@ -4,7 +4,13 @@ export default {
   components: {
     CartItem,
     },
-  props:['cart']
+  props: ['cart'],
+  computed: {
+    totalPrice() {
+      return this.cart.reduce((total, dessert) => total + dessert.price, 0).toFixed(2)
+    }
+  },
+
 };
 </script>
 
@@ -14,6 +20,12 @@ export default {
         <CartItem :item="item" />
       </li>
     </ul>
+    <div class="total">
+      <h3>Order Total</h3>
+      <h2>{{`$${totalPrice}`}}</h2>
+    </div>
+
+    <button>Confirm order</button>
 </template>
 
 <style scoped>
@@ -22,5 +34,23 @@ ul{
     display: flex;
     flex-direction: column;
     gap: 10px;
+}
+.total {
+  border: 1px solid black;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+button {
+  background-color: #ef4444;
+  border: none;
+  padding: 0.5rem 1rem;
+  border-radius: 10px;
+  cursor: pointer;
+}
+
+button:hover {
+  background-color: red;
 }
 </style>
