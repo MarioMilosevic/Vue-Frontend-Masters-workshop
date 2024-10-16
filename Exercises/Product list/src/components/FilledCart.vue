178b7f8086161b27a48a3ba4d51cb3ebb77cd92e
Mario Milosevic <128989997+MarioMilosevic@users.noreply.github.com>
2024-10-16 11:24:21 +0200

    Add modal component

--- Exercises/Product list/src/components/FilledCart.vue
+++ Exercises/Product list/src/components/FilledCart.vue
@@ -1,5 +1,6 @@
 <script>
 import CartItem from "./CartItem.vue";
+import Modal from "./Modal.vue";
 export default {
   data() {
     return {
@@ -8,6 +9,7 @@ export default {
   },
   components: {
     CartItem,
+    Modal,
   },
   props: ["cart"],
   inject: ["emptyCart"],
@@ -40,16 +42,7 @@ export default {
   <button @click="open = true">Confirm order</button>
 
   <Teleport to="body">
-    <div v-if="open" class="modal">
-      <h1>Order confirmed</h1>
-      <h2>We hope you enjoy your food!</h2>
-      <ul class="modalList">
-        <li v-for="item in cart" :key="item.name">
-          <CartItem :item :showButton="false" />
-        </li>
-      </ul>
-      <button @click="startNewOrder()">Start new order</button>
-    </div>
+    <Modal :open="open" :cart="cart" @start-new-order="startNewOrder" />
   </Teleport>
 </template>
 
@@ -76,27 +69,5 @@ button {
 
 button:hover {
   background-color: red;
-}
-
-.modal {
-  border: 1px solid black;
-  position: fixed;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  width: 500px;
-  height: 500px;
-  background-color: white;
-  padding: 1rem;
-  display: flex;
-  flex-direction: column;
-  gap: 1rem;
-}
-
-.modalList {
-  list-style: none;
-  display: flex;
-  flex-direction: column;
-  gap: 1rem;
 }
 </style>
---
+++ Exercises/Product list/src/components/Modal.vue
@@ -0,0 +1,64 @@
+<template>
+  <div v-if="open" class="modal">
+    <h1>Order confirmed</h1>
+    <h2>We hope you enjoy your food!</h2>
+    <ul class="modalList">
+      <li v-for="item in cart" :key="item.name">
+        <CartItem :item :showButton="false" />
+      </li>
+    </ul>
+    <button @click="reset">Start new order</button>
+  </div>
+</template>
+
+<script>
+import CartItem from "./CartItem.vue";
+export default {
+  name: "Modal",
+  props: ["cart", "open"],
+  components: {
+    CartItem,
+  },
+  methods: {
+    reset() {
+      this.$emit("start-new-order");
+    },
+  },
+};
+</script>
+
+<style scoped>
+.modal {
+  border: 1px solid black;
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  width: 500px;
+  height: 500px;
+  background-color: white;
+  padding: 1rem;
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+}
+
+.modalList {
+  list-style: none;
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+}
+
+button {
+  background-color: #ef4444;
+  border: none;
+  padding: 0.5rem 1rem;
+  border-radius: 10px;
+  cursor: pointer;
+}
+
+button:hover {
+  background-color: red;
+}
+</style>
