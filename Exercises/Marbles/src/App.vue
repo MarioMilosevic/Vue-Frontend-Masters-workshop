a49a2e97cca04101f71adfaf71b994ee3307403c
Mario Milosevic <128989997+MarioMilosevic@users.noreply.github.com>
2025-02-16 20:36:10 +0100

    Add template code

--- Exercises/Marbles/src/App.vue
+++ Exercises/Marbles/src/App.vue
@@ -1,30 +1,50 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <!-- <div>
+    <router-link :to="{path:'/user', query:{param1:'test1'}}">Home</router-link>
+  </div> -->
+  <div class="buttons">
+    <button @click="startInterval">-</button>
+    <h2>{{ count }}</h2>
+    <button @click="startInterval" @mousedown="mouseDown" @mouseup="stopInterval">+</button>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
+<script setup lang="ts">
+import { ref } from "vue"
+
+const count = ref(0)
+let intervalId = null
+const timeoutId = null
+
+const increment = () => {
+  count.value++
+}
+
+const startInterval = () => {
+  if (!intervalId) {
+    intervalId = setInterval(increment, 1000)
+  }
+}
+
+const stopInterval = () => {
+  clearInterval(intervalId)
+  intervalId = null
+}
+
+const mouseDown = () => {
+  if (!timeoutId) {
+    setTimeout(() => {
+      startInterval()
+    }, 500)
+  }
+  console.log("mis dole")
+}
+</script>
+
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+.buttons {
+  display: flex;
+  align-items: center;
+  gap: 1rem;
 }
 </style>
